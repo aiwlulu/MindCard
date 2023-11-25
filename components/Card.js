57662647e7ba330fab9b91e45c8fe18765ba014f
Lulu <s9341729@gmail.com>
2023-11-25 14:42:41 +0800

    Replace react-beautiful-dnd with @hello-pangea/dnd to resolve warnings

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import { MindmapContext } from "@/lib/store/mindmap-context";
 
 const Card = ({ currentMindmapId }) => {
