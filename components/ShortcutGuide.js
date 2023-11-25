9be2c66a59b874bc72991bf9c1706ebd2561afcf
Lulu <s9341729@gmail.com>
2023-11-25 13:22:15 +0800

    Refine ShortcutGuide: Adjust style and content

--- components/ShortcutGuide.js
+++ components/ShortcutGuide.js
@@ -19,26 +19,19 @@ const ShortcutGuide = () => {
       {showGuide && (
         <div className="mt-4 p-4 bg-gray-800 text-white rounded-lg shadow-xl">
           <ul className="list-disc list-inside space-y-2">
-            <li className="font-medium">Enter - Insert Sibling Node</li>
-            <li className="font-medium">Tab - Insert Child Node</li>
-            <li className="font-medium">F1 - Center the Map</li>
-            <li className="font-medium">F2 - Edit Selected Node</li>
-            <li className="font-medium">Del - Delete Selected Node</li>
-            <li className="font-medium">
-              ↑ - Select the Previous Sibling Node
-            </li>
-            <li className="font-medium">↓ - Select the Next Sibling Node</li>
-            <li className="font-medium">
-              ← / → - Select Parent or First Child
-            </li>
-            <li className="font-medium">PageUp / Alt + ↑ - Move Up Node</li>
-            <li className="font-medium">PageDown / Alt + ↓ - Move Down Node</li>
-            <li className="font-medium">Ctrl + S - Save Mind Map</li>
-            <li className="font-medium">Ctrl + C - Copy Selected Node</li>
-            <li className="font-medium">Ctrl + V - Paste the Copied Node</li>
-            <li className="font-medium">Ctrl + “+”- Zoom In Mind Map</li>
-            <li className="font-medium">Ctrl + “-” - Zoom Out Mind Map</li>
-            <li className="font-medium">Ctrl + 0 - Reset Zoom Level</li>
+            <li className="font-normal">Enter - Insert Sibling Node</li>
+            <li className="font-normal">Tab - Insert Child Node</li>
+            <li className="font-normal">Del - Delete Selected Node</li>
+            <li className="font-normal">PageUp / Alt + ↑ - Move Up Node</li>
+            <li className="font-normal">PageDown / Alt + ↓ - Move Down Node</li>
+            <li className="font-normal">Ctrl + S - Save Mind Map</li>
+            <li className="font-normal">Ctrl + C - Copy Selected Node</li>
+            <li className="font-normal">Ctrl + V - Paste the Copied Node</li>
+            <li className="font-normal">Ctrl + “+”- Zoom In Mind Map</li>
+            <li className="font-normal">Ctrl + “-” - Zoom Out Mind Map</li>
+            <li className="font-normal">Ctrl + 0 - Reset Zoom Level</li>
+            <li className="font-normal">F1 - Center the Map</li>
+            <li className="font-normal">F2 - Edit Selected Node</li>
           </ul>
         </div>
       )}
